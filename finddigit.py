a94f37ed3593c39b026ac0a97b1b4a6a57e22543
SREETAMA <84221984+dassreetama@users.noreply.github.com>
2022-10-29 14:06:42 +0530

    Create finddigit.py

---
+++ finddigit.py
@@ -0,0 +1,16 @@
+# Python3 code to demonstrate
+# getting numbers from string
+# using List comprehension + isdigit() +split()
+
+# initializing string
+test_string = "There are 2 apples for 4 persons"
+
+# printing original string
+print("The original string : " + test_string)
+
+# using List comprehension + isdigit() +split()
+# getting numbers from string
+res = [int(i) for i in test_string.split() if i.isdigit()]
+
+# print result
+print("The numbers list is : " + str(res))
